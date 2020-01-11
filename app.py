ff39d8d189f643c3baf2c48781dcde8ecc373c70
Brad Larson <bhlarson@gmail.com>
2020-01-11 09:11:48 -0800

    Removing image processing from camrea class

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
-from importlib import import_module
 import os
+import cv2
+import numpy as np
+from importlib import import_module
 from flask import Flask, render_template, Response
 from camera_opencv import Camera
+
 
 
 app = Flask(__name__)
@@ -17,7 +20,22 @@ def index():
 def gen(camera):
     """Video streaming generator function."""
     while True:
-        frame = camera.get_frame()
+        img = camera.get_frame()
+        img = cv2.flip(img, +1)
+
+        imgShape = img.shape
+
+        color =  (0,255,0)
+        thickness =  3
+        center = np.array([imgShape[1]/2, imgShape[0]/2])
+        d =  np.array([128,128])
+        p1 = tuple((center-d).astype(int))
+        p2 = tuple((center+d).astype(int))
+        cv2.rectangle(img,p1,p2,color,thickness)
+
+        # encode as a jpeg image and return it
+        frame = cv2.imencode('.jpg', img)[1].tobytes()
+
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
--- camera_opencv.py
+++ camera_opencv.py
@@ -1,5 +1,4 @@
 import cv2
-import numpy as np
 from base_camera import BaseCamera
 
 
@@ -20,15 +19,4 @@ class Camera(BaseCamera):
             # read current frame
             _, img = camera.read()
 
-            imgShape = img.shape
-
-            color =  (0,255,0)
-            thickness =  3
-            center = np.array([imgShape[1]/2, imgShape[0]/2])
-            d =  np.array([128,128])
-            p1 = tuple((center-d).astype(int))
-            p2 = tuple((center+d).astype(int))
-            cv2.rectangle(img,p1,p2,color,thickness)
-
-            # encode as a jpeg image and return it
-            yield cv2.imencode('.jpg', img)[1].tobytes()
+            yield img
