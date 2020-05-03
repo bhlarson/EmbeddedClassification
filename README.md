89b2c21b1c4035aadbc2b5c2ca49356393ba8762
Brad Larson <brad.larson1@thermofisher.com>
2020-05-02 23:50:26 -0700

    Adding infer_imdb.py

--- README.md
+++ README.md
@@ -1,37 +1,26 @@
 # EmbeddedClassification
 Embedded image classification based on tensorflow model
+Resnet model trained on IMDB dataset to classify image age and gender
 
-Build training docker image and add to kubernetes registry:
-> docker build --rm -f "dockerfile_fcntrain" -t localhost:32000/cl:latest "."
-Push image into registry:
-> docker push localhost:32000/cl
-
-view registry in browser through the URL:
-http://192.168.1.66:32000/v2/_catalog 
-
-log in to kubernetes dashboard:
-
-https://192.168.1.66:32090/#!/login
-
+Dataset: https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/
 
 1. Build docker image
 > docker build --rm -f dockerfile -t ec:latest context
 
-1. Stop and remove the current cl
-   > docker container ls
-   note the IMAGE ID for cl
-    > docker stop <CONTAINER ID>
-
-1. Load docker image
-   > docker load -i containers/cl.tar 
 1. Run docker image
-   > docker run --rm -it -v "$(pwd):/app" -v "/store/Datasets/imdb:/trainingset" -v "/store/Datasets:/store/Datasets" --gpus '"device=0"' -p 6006:6006/tcp -p 3000:3000 ec:latest
-1. To increase logging of TRT conversion, launch python with the desired logging levels
-   > TF_CPP_VMODULE=segment=1,convert_graph=1,convert_nodes=1,trt_engine_op=1 python savemodelv2.py > conversion.txt 2>&1
-1.  
-   > docker run --rm -it -v "$(pwd):/app" -v "/store/Datasets/imdb:/trainingset" -v "/store/Datasets:/store/Datasets" --gpus '"device=0"' -p 6006:6006/tcp ec:latest
+   > docker run --gpus '"device=0"' -it --rm -v "$(pwd):/app" -v "/store:/store" -p 6006:6006/tcp -p 3000:3000 ec:latest
 
-> docker run --gpus '"device=0"' -it --rm -v "$(pwd):/app/ec" -v "/store/Datasets/imdb:/trainingset" -v "/store/Datasets:/store/Datasets" -p 6006:6006/tcp -p 3000:3000 nvcr.io/nvidia/tensorrt:20.03-py3
+1. In docker container, convert dataset into record:
+   > python makecrecord.py
+1. In docker container, train network:
+   > python train_imdbv2.py
+1. In docker container, test network:
+   > python test_imdb.py # Not yet updated to TF 2
+1. In docker container, evaluate images:
+   > python infer_imdb.py
+
+
+
 
 Docker tensorrt
 > docker build --rm -f imgdfn/dockerfile -t trt01:latest imgdfn/context
@@ -43,6 +32,6 @@ Run TRT image:
 
 > docker run --gpus '"device=0"' -it --rm -v "$(pwd):/app" -v "/store/Datasets/imdb:/trainingset" -v "/store/Datasets:/store/Datasets" -p 3000:3000 trt01:latest
 
-> docker run --gpus '"device=0"' -it --rm -v "$(pwd):/app" -v "/store:/store" -p 6006:6006/tcp -p 3000:3000 ec:latest
+
 
 python -m tensorflow.python.tools.freeze_graph --input_saved_model_dir SAVED_MODEL_DIR
---
+++ infer_imdb.py
@@ -0,0 +1,133 @@
+"""Train a Resnet model for age classification and gender regression from the imdb dataset."""
+#from __future__ import absolute_import
+#from __future__ import division
+#from __future__ import print_function
+
+import argparse
+import os
+import sys
+import shutil
+import glob
+import cv2
+import datetime
+#import tensorrt as trt
+#import uff
+#import pycuda.driver as cuda
+#import pycuda.autoinit
+import tensorflow as tf
+
+print('Python Version {}'.format(sys.version))
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--debug', type=bool, default=False, help='Wait for debugge attach')
+
+parser.add_argument('--model', type=str, default='./saved_model/1588440354',
+                    help='Base directory for the model.')
+
+parser.add_argument('--data_dir', type=str, 
+                    default='/store/Datasets/imdb/imdb_crop/00/',
+                    #default='C:\\data\\datasets\\imdb',
+                    help='Path to the directory containing the imdb data tf record.')
+
+parser.add_argument('--match', type=str, default='*',
+                    help='File wildcard')
+
+# Pre-trained models: https://github.com/tensorflow/models/blob/master/research/slim/README.md
+parser.add_argument('--pre_trained_model', type=str, 
+                    default='/store/training/resnet_v2_101_2017_04_14/resnet_v2_101.ckpt',
+                    #default='C:\\data\\training\\resnet_v2_101_2017_04_14\\resnet_v2_101.ckpt',
+                    help='Path to the pre-trained model checkpoint.')
+
+
+_NUM_CLASSES = 21
+_HEIGHT = 200
+_WIDTH = 200
+_DEPTH = 3
+_MIN_SCALE = 0.5
+_MAX_SCALE = 2.0
+_IGNORE_LABEL = 255
+
+_POWER = 0.9
+_MOMENTUM = 0.9
+
+_BATCH_NORM_DECAY = 0.9997
+
+_NUM_IMAGES = {
+    'train': 10582,
+    'validation': 1449,
+}
+
+
+def get_filenames(data_dir, ext):
+    """Return a list of filenames.
+
+    Args:
+        is_training: A boolean denoting whether the input is for training.
+        data_dir: path to the the directory containing the input data.
+
+    Returns:
+        A list of file names.
+    """
+    return glob.glob(os.path.join(data_dir, ext))
+
+def build_engine(FLAGS):
+    uff_model = uff.from_tensorflow_frozen_model(FLAGS.model, debug_mode=True, return_graph_info=True)
+    with trt.Builder(TRT_LOGGER) as builder, builder.create_network() as network, trt.UffParser() as parser:
+        builder.max_workspace_size = 1 << 30
+        builder.fp16_mode = True
+        builder.max_batch_size = 1
+        parser.register_input("Input", (3, _WIDTH, _HEIGHT))
+        parser.register_output("MarkOutput_0")
+        parser.parse(uff_model_path, network)
+        
+        print("Building TensorRT engine, this may take a few minutes...")
+        trt_engine = builder.build_cuda_engine(network)
+    
+
+def main(FLAGS):
+    #TRT_LOGGER = trt.Logger(trt.Logger.VERBOSE)
+
+    #engine = build_engine(FLAGS)
+
+    loaded = tf.saved_model.load(FLAGS.model)
+    print(list(loaded.signatures.keys()))
+    infer = loaded.signatures["serving_default"]
+    print(infer.structured_outputs)
+    print (infer.inputs[0])
+    imgs = get_filenames(FLAGS.data_dir, FLAGS.match)
+
+    img = cv2.imread(imgs[0])
+    tfimg = tf.image.resize_with_crop_or_pad(tf.constant(img), _HEIGHT, _WIDTH)
+    outputs = infer(tfimg)
+
+    start_time = datetime.datetime.now()
+    for i, imfile in enumerate(imgs):
+        img = cv2.imread(imfile)
+        tfimg = tf.image.resize_with_crop_or_pad(tf.constant(img), _HEIGHT, _WIDTH)
+        outputs = infer(tfimg)
+
+        #print('{}: pred_age {}, pred_gender {}, '.format(i, outputs['pred_age'].numpy()[0,0],outputs['pred_gender'].numpy()[0]))
+    analysis_done = datetime.datetime.now()
+    total_time = (analysis_done-start_time).total_seconds()
+
+    print('average image time {}'.format(total_time/len(imgs)))
+
+if __name__ == '__main__':
+    FLAGS, unparsed = parser.parse_known_args()
+
+    if FLAGS.debug:
+        # https://code.visualstudio.com/docs/python/debugging#_remote-debugging
+        # Launch applicaiton on remote computer: 
+        # > python3 -m ptvsd --host 0.0.0.0 --port 3000 --wait predict_imdb.py
+        import ptvsd
+        # Allow other computers to attach to ptvsd at this IP address and port.
+        ptvsd.enable_attach(address=('0.0.0.0', 3000), redirect_output=True)
+        # Pause the program until a remote debugger is attached
+        print("Wait for debugger attach")
+        ptvsd.wait_for_attach()
+        print("Debugger Attached")
+
+    FLAGS, unparsed = parser.parse_known_args()
+    main(FLAGS)
+    print('complete')
